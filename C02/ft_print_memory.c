73dcaba41118f75849c68b8277986ec09d89c108
hungeredJims <sp0943@cau.ac.kr>
2023-07-28 22:40:20 +0900

    12 mod

--- C02/ft_print_memory.c
+++ C02/ft_print_memory.c
@@ -11,33 +11,78 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 
-void	to_hex(long long ad)
+void	hex_addr(long long ad, unsigned int n)
 {
-	if (ad <16)
-		write (1, &"0123456789abcdef"[ad], 1);
-	else
+	while(ad == 0 && n < 16)
 	{
-		to_hex (ad / 16);
-		to_hex (ad % 16);
+		write (1, &"0123456789abcdef"[ad], 1);
+		n++;
 	}
+	if (ad != 0)
+	{
+		hex_addr (ad / 16, ++n);
+		write (1, &"0123456789abcdef"[ad % 16], 1);
+	}
+}
+
+void hex_con(void *addr, unsigned int i, unsigned int size){
+	unsigned int ix;
+	
+	ix = i;
+	while(i < ix + 16){
+		if(i>=size)
+			write(1, "  ", 2);
+		else
+		{
+			write(1, &"0123456789abcdef"[(*((unsigned char *)addr +i))/16], 1);
+			write(1, &"0123456789abcdef"[(*((unsigned char *)addr +i))%16], 1);
+		}
+		if(i%2)
+			write(1, " ", 1);
+		i++;
+	}
+}
+
+void print_able(void *addr, unsigned int i, unsigned int size){
+	unsigned int ix;
+	
+	ix = i;
+	while(i < ix + 16){
+		if(i>=size)
+			write(1, " ", 1);
+		else if (*(char *)(addr + i)>=32 && *(char *)(addr + i)<= 126)
+			write(1, (char *)(addr + i), 1);
+		else
+			write(1, ".", 1);
+		i++;
+	}
+	write(1, "\n", 1);
 }
 
 void	*ft_print_memory(void *addr, unsigned int size)
 {
-	int i;
-
+	unsigned int i;
+	
 	i = 0;
-	while ((size) && (addr[i]))
+	while (i < size)
 	{
-		to_hex ((long long)&addr[i]);
+		hex_addr ((long long)&addr[i], 0);
 		write (1, ": ", 2);
+		hex_con (addr, i, size);
+		print_able(addr, i, size);
 		i += 16;
 	}
+	return (addr);
 }
 
-int main (void)
-{
-	char s1[] = "iloveyou iloveyou iloveyou iloveyou iloveyou iloveyou";
-	ft_print_memory(s1, 56);
+int main() {
+	char *test = "\220Salut lasd wlaks' \320asdkas\220da klaskd\1 aksdjladkjwdn andawkw\1\1asdasdajwkd";
+	char *test30 = "\0";
+	ft_print_memory(test, strlen(test));
+	printf("\n\n");
+	ft_print_memory(test30, strlen(test30));
+	printf("\n\n------------------------------\n\n");
 }
